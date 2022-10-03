62010d7e0b60475a9bbb3312276ab467fc2e7dd3
康宏嘉 <redalertkhj@live.cn>
2022-10-03 18:04:01 +0800

    fix: fix compiling errors.

--- vendor/luabins/src/luaheaders.h
+++ vendor/luabins/src/luaheaders.h
@@ -11,4 +11,26 @@ extern "C" {
 }
 #endif
 
+/*
+** compatibility with Lua 5.2
+*/
+#if (LUA_VERSION_NUM >= 502)
+
+#undef lua_equal
+#define lua_equal(L,idx1,idx2)  lua_compare(L,(idx1),(idx2),LUA_OPEQ)
+
+#undef lua_getfenv
+#define lua_getfenv	lua_getuservalue
+#undef lua_setfenv
+#define lua_setfenv	lua_setuservalue
+
+#undef lua_objlen
+#define lua_objlen	lua_rawlen
+
+#undef luaL_register
+#define luaL_register(L,n,f) \
+	{ if ((n) == NULL) luaL_setfuncs(L,f,0); else luaL_newlib(L,f); }
+
+#endif
+
 #endif /* LUABINS_LUAHEADERS_H_INCLUDED_ */
